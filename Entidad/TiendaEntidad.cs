9996b20540392ff2aa6c923c7df9783f174c5ac4
kannoncoding <codingadaptations@gmail.com>
2025-02-23 01:24:26 -0600

    Eliminando archivos no requeridos

---
+++ Entidad/TiendaEntidad.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _45GAMES4U_Inventory.Entidad
+{
+    class TiendaEntidad
+    {
+    }
+}
